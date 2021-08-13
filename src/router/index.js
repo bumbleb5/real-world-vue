4f560464cdfcf4cdbfe9a51dcd1b9a140f16b9c7
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-08-13 08:26:56 -0500

    found router issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,35 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import EventList from '../views/EventList.vue';
+import EventCreate from '../views/EventCreate.vue';
+import EventShow from '../views/EventShow.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'event-list',
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/event',
+    name: 'event-show',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    // },
+    component: EventShow,
+    props: true,
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate,
   },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
